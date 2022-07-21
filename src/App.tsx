5b5ed7cf7961958891789b4fd7f58eebb4d4fcda
srirachy <caseyereyes@gmail.com>
2022-07-21 16:09:24 -0700

    docs(boilerplate) add boilerplate document files for a good baseline project ie linter, prettier, error boundary, tsconfig, and removed unneeded files

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // write component tests here
   return (
     <div className="App">
       <header className="App-header">

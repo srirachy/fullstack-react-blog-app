446099ed02dfc15de4d2ce4bf0fd9560b3996887
Wosen100 <83084085+Wosen100@users.noreply.github.com>
2022-08-03 22:03:36 -0400

    No change has made

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import AddPost from './pages/AddPost';
 import SignUp from './pages/signup';
 import Main from './pages/Main';
 import ViewPost from './pages/ViewPost';
-import SubmitPost from './pages/SubmitPost';
+// import SubmitPost from './pages/SubmitPost';
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Main />} />
@@ -17,7 +17,7 @@ function App() {
         <Route path="/AddPost" element={<AddPost />} />
         <Route path="/sign-up" element={<SignUp />} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
 

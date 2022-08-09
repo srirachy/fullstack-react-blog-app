a55c12c4c6082b5e02913060adcaa6422debc293
Richie559 <richardnavarro559@gmail.com>
2022-08-09 15:43:39 -0700

    feat(viewcommunity) We figured out how to filter communities

--- client/src/components/Navbar/NavbarElements.jsx
+++ client/src/components/Navbar/NavbarElements.jsx
@@ -27,21 +27,6 @@ export const NavLink = styled(Link)`
   }
 `;
 
-// export const Bars = styled(FaBars)`
-//   display: none;
-//   color: #fff;
-
-//   @media screen and (max-width: 768px) {
-//     display: block;
-//     position: absolute;
-//     top: 0;
-//     right: 0;
-//     transform: translate(-100%, 75%);
-//     font-size: 1.8rem;
-//     cursor: pointer;
-//   }
-// `;
-
 export const NavMenu = styled.div`
   display: flex;
   align-items: center;
